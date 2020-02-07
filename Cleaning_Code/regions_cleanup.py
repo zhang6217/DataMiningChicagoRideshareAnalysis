59805a0ad83c07214f0cfdc54474a3350c11e11a
Lucia Ronchi <lronchi@uchicago.edu>
2020-02-07 17:38:41 -0600

    Changed columns to SE and NE

--- Cleaning_Code/regions_cleanup.py
+++ Cleaning_Code/regions_cleanup.py
@@ -2,15 +2,17 @@ import pandas as pd
 
 regions_df = pd.read_csv("~/regions.csv")
 
-regions_df.rename(columns = {'REGIONID':'region_id'}, inplace = True) 
+regions_df.rename(columns = {'REGIONID':'region_id'}, inplace = True)
 
 regions_df.columns = map(str.lower, regions_df.columns)
 
 regions_df.columns = regions_df.columns.to_series().apply(lambda x: x.strip())
 
-regions_df = regions_df.drop(["current_speed","last_updated","description", "east", "south"], axis = 1)
+#regions_df = regions_df.drop(["current_speed","last_updated","description", "east", "south"], axis = 1)
 
-regions_df = regions_df[["region_id","region","west","north"]]
+regions_df = regions_df[["region_id","region","nw_location","se_location"]]
+
+regions_df.drop_duplicates(inplace = True)
 
 regions_df.to_csv(r"~/clean_regions.csv", index = False) # To be used in other calculations
 
