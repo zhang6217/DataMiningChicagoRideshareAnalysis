5a9fc46e6d9fe5761f9aa07e9c5d1b21b14d431f
Lucia Ronchi <lronchi@uchicago.edu>
2020-02-08 18:57:10 -0600

    Computing latitude and longitude centrois for each region

--- Cleaning_Code/regions_cleanup.py
+++ Cleaning_Code/regions_cleanup.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 regions_df = pd.read_csv("~/regions.csv")
 
@@ -13,6 +14,42 @@ regions_df.columns = regions_df.columns.to_series().apply(lambda x: x.strip())
 regions_df = regions_df[["region_id","region","nw_location","se_location"]]
 
 regions_df.drop_duplicates(inplace = True)
+
+# Create empty columns for lats and longs
+regions_df["lat_nw"] = ""
+regions_df["long_nw"] = ""
+regions_df["lat_se"] = ""
+regions_df["long_se"] = ""
+
+# Fill empty columns
+for row in range(len(regions_df.index)):
+
+    # NW latitude
+    start = regions_df.iloc[row, 2].find("(") + 1
+    end = regions_df.iloc[row, 2].find(" ", 6)
+    regions_df.iloc[row, 4] = float(regions_df.iloc[row, 2][start:end])
+
+    # NW longitude
+    start = regions_df.iloc[row, 2].find(" ", 6) + 1
+    end = regions_df.iloc[row, 2].find(")")
+    regions_df.iloc[row, 5] = float(regions_df.iloc[row, 2][start:end])
+
+    # SE latitude
+    start = regions_df.iloc[row, 3].find("(") + 1
+    end = regions_df.iloc[row, 3].find(" ", 6)
+    regions_df.iloc[row, 6] = float(regions_df.iloc[row, 3][start:end])
+
+    # SE longitude
+    start = regions_df.iloc[row, 3].find(" ", 6) + 1
+    end = regions_df.iloc[row, 3].find(")")
+    regions_df.iloc[row, 7] = float(regions_df.iloc[row, 3][start:end])
+
+# Compute centroids
+regions_df["lat_centroid"] = (regions_df.lat_nw+regions_df.lat_se)/2
+regions_df["long_centroid"] = (regions_df.long_nw+regions_df.long_se)/2
+
+# Keep only relevant columns
+regions_df = regions_df[["region_id","region","lat_centroid", "long_centroid"]]
 
 regions_df.to_csv(r"~/clean_regions.csv", index = False) # To be used in other calculations
 
