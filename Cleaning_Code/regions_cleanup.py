717e38e480c07bbccaf6b0933bd0aaffbe36cca2
Lucia Ronchi <lronchi@uchicago.edu>
2020-02-10 00:01:44 -0600

    Updated with comments by Luis

--- Cleaning_Code/regions_cleanup.py
+++ Cleaning_Code/regions_cleanup.py
@@ -8,11 +8,9 @@ regions_df.columns = map(str.lower, regions_df.columns)
 
 regions_df.columns = regions_df.columns.to_series().apply(lambda x: x.strip())
 
-#regions_df = regions_df.drop(["current_speed","last_updated","description", "east", "south"], axis = 1)
-
 regions_df = regions_df[["region_id","region","nw_location","se_location"]]
 
-regions_df.drop_duplicates(inplace = True)
+regions_df.drop_duplicates(subset = "region_id", inplace = True)
 
 # Create empty columns for lats and longs
 regions_df["lat_nw"] = ""
@@ -51,5 +49,3 @@ regions_df["long_centroid"] = (regions_df.long_nw+regions_df.long_se)/2
 regions_df = regions_df[["region_id","region","lat_centroid", "long_centroid"]]
 
 regions_df.to_csv(r"~/clean_regions.csv", index = False) # To be used in other calculations
-
-regions_df.to_csv(r"~/headerless_clean_regions.csv", index = False, header = False) # Actual DW table for dim_base_region
