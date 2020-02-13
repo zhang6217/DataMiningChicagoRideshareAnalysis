06918234d0880a0a41843053597dee5870ecbac9
Luis Flosi <luisflosi@uchicago.edu>
2020-02-13 10:04:41 -0600

    Removed unnecessary NAs

--- Cleaning_Code/trips_merge.py
+++ Cleaning_Code/trips_merge.py
@@ -5,11 +5,11 @@ rideshare_cleaned = pd.read_csv("~/clean_rideshare_trips.csv")
 
 trips_final = pd.concat((taxi_cleaned,rideshare_cleaned), ignore_index = True)
 
-trips_final = trips_final.drop(["Unnamed: 0", "start_census_tract", "end_census_tract"], axis = 1, errors = "ignore", inplace = True)
+trips_final = trips_final.drop(["Unnamed: 0", "start_census_tract", "end_census_tract"], axis = 1, errors = "ignore")
 
 trips_final.to_csv(r"~/cleaned_full_trips.csv") # will be used for further calculations
 
-trips_final[["tolls","extra_charges"]] = trips_final[["tolls","extra_charges"]].fillna(0, inplace = True)
+trips_final[["tolls","extra_charges"]] = trips_final[["tolls","extra_charges"]].fillna(0)
 
 trips_final.dropna(axis = 0, inplace = True) # Removing trips that involve pickup or dropoff outside Chicago (have null centroids)
 
