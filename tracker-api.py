19e00b28c1462ce6a49ffee8a0b2f76bb77f2c3e
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2020-03-11 14:43:24 -0500

    Final reorganization + deleted unnecessary files

--- tracker-api.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sodapy import Socrata
-
-client = Socrata("data.cityofchicago.org", None)
-
-results = client.get("77hq-huss")
-results_df = pd.DataFrame.from_records(results)
